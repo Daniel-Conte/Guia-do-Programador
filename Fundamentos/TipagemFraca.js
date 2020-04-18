689e208a78c6c2ae0f947f8ff7e71c9f77c80737
Daniel Conte <182736@upf.br>
2020-04-18 00:04:05 -0300

    atualizando

---
+++ Fundamentos/TipagemFraca.js
@@ -0,0 +1,6 @@
+let qualquer = "Legal"
+console.log(qualquer)
+console.log(typeof qualquer) //funcao para verificar que tipo é a variavel/constante
+qualquer = 3.1415
+console.log(qualquer)
+console.log(typeof qualquer)
