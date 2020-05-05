b454f540e8f9da2e60f11ab03a3a05290b3663ec
Daniel Conte <182736@upf.br>
2020-05-05 11:00:54 -0300

    atualizando

---
+++ HTML/exercicios/form/server.js
@@ -0,0 +1,12 @@
+const express = require('express')              // REVER A AULA DE FORMULARIOS
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/usuarios', (req, resp) => {
+    console.log(req.body)
+    resp.send('<h1>Parabéns</h1>')
+})
+
+app.listen(3003)
