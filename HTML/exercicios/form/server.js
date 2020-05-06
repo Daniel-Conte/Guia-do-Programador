d1883b41b52f0e44e5ca82995c1a3f72232ef9ba
Daniel Conte <182736@upf.br>
2020-05-06 00:34:52 -0300

    atualizando

--- HTML/exercicios/form/server.js
+++ HTML/exercicios/form/server.js
@@ -6,7 +6,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Parabéns</h1>')
+    resp.send('<h1>Parabéns. Usuário incluido!</h1>')
+})
+
+app.post('/usuarios/:id', (req, resp) => {  //":id" é definido como parametro
+    console.log(req.params.id)  //e mostra aqui
+    resp.send('<h1>Parabéns. Usuário Alterado!</h1>')
 })
 
 app.listen(3003)
