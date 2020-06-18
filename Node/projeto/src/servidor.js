75cf0d5229fcf419e171a493cf79a6e08d112b52
Daniel Conte <182736@upf.br>
2020-06-18 11:04:05 -0300

    atualizando

---
+++ Node/projeto/src/servidor.js
@@ -0,0 +1,4 @@
+const porta = 3003 //porta é um "local" em que tal processo está sendo executado dentro da máquina. IP é o endereço da máquina e PORTA é o endereço do processo dentro da máquina
+
+const express = require('express')
+const app = express()
