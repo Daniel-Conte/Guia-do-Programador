fde05012b4dbe0650dec24903754eb52ed377ef6
Daniel Conte <182736@upf.br>
2020-08-04 10:28:09 -0300

    Gulp CSS e SCSS

---
+++ WEB/Gulp/css/gulpfile.js
@@ -0,0 +1,22 @@
+const { series, parallel } = require('gulp')
+const gulp = require('gulp')
+const sass = require('gulp-sass') // Converte SCSS para CSS
+const uglifyCss = require('gulp-uglifycss') // Minifica arquivos CSS
+const concat = require('gulp-concat')
+
+function transformacaoCSS() {
+    return gulp.src('src/sass/index.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(uglifyCss({
+            "uglyComments": true
+        }))
+        .pipe(concat('estilo.min.css'))
+        .pipe(gulp.dest('build/css'))
+}
+
+function copiarHTML() {
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('build'))
+}
+
+exports.default = parallel(transformacaoCSS, copiarHTML)
