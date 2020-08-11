aa351330682719bd35bb3003a5c5d51ae22e7d34
Daniel Conte <182736@upf.br>
2020-08-11 09:52:46 -0300

    Webpack: producao e desenvolvimento

--- WEB/Webpack/webpack.config.js
+++ WEB/Webpack/webpack.config.js
@@ -1,9 +1,12 @@
+const modoDev = process.env.NODE_ENV !== 'production' // Pega a variavel declarada no script "build" no package.json pela dependencia "cross-env"
 const webpack = require('webpack')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin') // É usado para gerar um novo arquivo só com o Css
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 // Loaders não precisam ser chamados(require)
 
 module.exports = { // Toda a configuracao precisa estar dentro da exportacao
-    mode: 'development', // Seta o modo de como vai ser tratado os arquivos gerados
+    mode: modoDev ? 'development' : 'production', // Seta o modo de como vai ser tratado os arquivos gerados
         // 'development' -> Maior facilidade no momento de desenvolvimento do projeto. Tem source maps, mais facil de debugar, não é minificado...
         // 'production' -> Deixa o código pronto(estado final) para ir pro servidor. Faz o processo de minificacao, compilaçao...
     entry: './src/principal.js', // Define o arquivo de entrada para o WP(webpack) iniciar o empacotamento. Nesse arquivo precisa estar as importações de outros arquivos e dependencias
@@ -11,9 +14,20 @@ module.exports = { // Toda a configuracao precisa estar dentro da exportacao
         filename: 'principal.js', // Seta o nome do arquivo que vai ser gerado
         path: __dirname + '/public' // Seta o diretorio(junto com o nome da pasta) onde todos os arquivos gerados vão ficar. "__dirname" retorna o diretorio raiz de onde fica este arquivo
     },
+    optimization: { // Onde ficam todas as otimizações
+        minimizer: [ // Ativa só quando o modo for "production"
+            new OptimizeCSSAssetsPlugin({}) // Otimiza o CSS
+        ]
+    },
     plugins: [ // Todos os plugins ficam aqui
-        new MiniCssExtractPlugin({
+        new MiniCssExtractPlugin({ // É usado para gerar um novo arquivo só com o Css
             filename: "estilo.css" // Nome do arquivo que vai ser gerado
+        }),
+        new TerserPlugin({ // Otimiza o JS
+            parallel: true, // Faz a otimizacao mais rapido
+            terserOptions: {
+                ecma: 6
+            }
         })
     ],
     module: { // Webpack só interpreta JS e JSON. Para interpretar outros formatos precisa usar "loaders"
@@ -25,6 +39,6 @@ module.exports = { // Toda a configuracao precisa estar dentro da exportacao
                 'css-loader', // interpreta @import url()...
                 'sass-loader',
             ]
-        }]
+        }]        
     }
 }
