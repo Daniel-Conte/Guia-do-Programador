8a4bdcfb5bb15369058f31f8adb97f3e26a4e074
Daniel Conte <182736@upf.br>
2020-08-12 11:30:10 -0300

    Projeto Webpack: Sass e JS prontos, falta font-awesome e HTML

---
+++ WEB/Projetos/Webpack/spa/src/index.js
@@ -0,0 +1,4 @@
+require('./assets/js/navegacao.js')
+require('./assets/sass/index.scss')
+require('../node_modules/font-awesome/css/font-awesome.css')
+//require('index.html')
--- WEB/Projetos/Webpack/spa/webpack.config.js
+++ WEB/Projetos/Webpack/spa/webpack.config.js
@@ -1,5 +1,42 @@
-webpack = require('webpack')
+const webpack = require('webpack')
+const TerserPlugin = require('terser-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: './assets/js/app.min.js',
+        path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin({})
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '/assets/css/app.min.css'
+        }),
+        new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6
+            }
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+            ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [ 'file-loader' ]
+        }]
+    }
 }
