2d41b2953a14faad77db5e7c90c26bb3b92ee6bf
Daniel Conte <182736@upf.br>
2020-08-13 11:04:50 -0300

    Projeto Webpack: Projeto spa finalizado

--- WEB/Projetos/Webpack/spa/webpack.config.js
+++ WEB/Projetos/Webpack/spa/webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack')
 const TerserPlugin = require('terser-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
@@ -10,15 +11,44 @@ module.exports = {
         filename: './assets/js/app.min.js',
         path: __dirname + '/public'
     },
+    devServer: {
+        contentBase: './public',
+        port: 9000
+    },
     optimization: {
         minimizer: [
             new OptimizeCssAssetsPlugin({})
         ]
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new HtmlWebpackPlugin({
+            filename: '/paginas/cursos.html',
+            template: './src/paginas/cursos.html',
+            inject: false
+        }),
+        new HtmlWebpackPlugin({
+            filename: '/paginas/inicio.html',
+            template: './src/paginas/inicio.html',
+            inject: false
+        }),
+        new HtmlWebpackPlugin({
+            filename: '/paginas/sobre.html',
+            template: './src/paginas/sobre.html',
+            inject: false
+        }),
+        new HtmlWebpackPlugin({
+            filename: '/paginas/suporte.html',
+            template: './src/paginas/suporte.html',
+            inject: false
+        }),
+
         new MiniCssExtractPlugin({
             filename: '/assets/css/app.min.css'
         }),
+
         new TerserPlugin({
             parallel: true,
             terserOptions: {
@@ -35,8 +65,18 @@ module.exports = {
                 'sass-loader'
             ]
         }, {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [ 'file-loader' ]
+            test: /\.(png|jpg|gif)$/,
+            loader: 'file-loader',
+            options: {
+                name: 'logo.png',
+                outputPath: '/assets/imgs'
+            }
+        }, {
+            test: /\.(svg|eot|woff|woff2|ttf)$/,
+            loader: 'file-loader',
+            options: {
+                outputPath: '/assets/fonts'
+            }
         }]
     }
 }
