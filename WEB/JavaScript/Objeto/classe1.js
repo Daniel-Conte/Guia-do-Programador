3412b9f7cf29b8eae04e37e2404692e66fa33945
Daniel Conte <182736@upf.br>
2020-09-13 10:56:23 -0300

    JavaScript: Atualizando: classe1 e 2

--- WEB/JavaScript/Objeto/classe1.js
+++ WEB/JavaScript/Objeto/classe1.js
@@ -1,6 +1,19 @@
-class Lancamento { //Classes são tranformadas em funcoes construtoras
-    constructor(nome = 'Generico', valor = 0) { //Define os parametros
-        this.nome = nome //Adicionando o atributo "nome" ao obj que está sendo instanciado
+/*
+    - Classes são tranformadas em funcoes construtoras
+    - "class <nomeDaFunção>" cria uma nova classe
+        - O nome da função deve iniciar com letra MAIÚSCULA
+    - Classes NÃO fazem Hoisting, ou seja, você deve criar a classe ANTES de estanciá-la
+*/
+
+class Lancamento {
+    constructor(nome = 'Generico', valor = 0) {
+        /* "constructor(<parametros>)" é onde será iniciada as propriedades/atributos
+            - Todos os atributos que você quer que sejam acessados fora do constructor devem ser add ao "this"
+                - "this" é um objeto que está referenciando esta classe(Lancamento)
+                - Todos os atributos add no "this"(dentro do constructor) serão acessíveis em qualquer lugar dentro desta classe(Lancamento) usando "this.<atributo>"
+            - Quem está instanciando essa classe vai receber estes atributos
+        */
+        this.nome = nome
         this.valor = valor
     }
 }
@@ -12,8 +25,18 @@ class CicloFinanceiro {
         this.lancamentos = []
     }
 
-    addLancamentos(...lancamentos) {
-        lancamentos.forEach(l => this.lancamentos.push(l))//Esse this tá referenciando "lancamentos" do constructor
+    // É possível criar funções dentro de uma classe sem "function" no inicio
+        // Quem está instanciando essa classe vai receber estas funções    
+    addLancamentos(...lancamentos) { // Espalhando todos os parâmetros recebidos dentro de um array
+        lancamentos.forEach(l => this.lancamentos.push(l)) // Esse this está referenciando "lancamentos" do constructor
+            // (ou seja) Acessando o atributo "lancamentos" dentro do objeto "this"
+    }
+
+    // Para quem está instanciando essa classe NÃO receber uma função, usa-se "static"
+        // Essa função ficará privada à esta classe
+        // Só poderá ser acessada assim -> CicloFinanceiro.Teste()
+    static Teste() {
+        console.log("Teste...")
     }
 
     sumario() {
@@ -25,7 +48,7 @@ class CicloFinanceiro {
     }
 }
 
-const salario = new Lancamento('Salario', 45000)
+const salario = new Lancamento('Salario', 45000) // Esses parâmetros serão enviados para o "constructor()" da classe
 const contaDeLuz = new Lancamento('Luz', -220)
 
 const contas = new CicloFinanceiro(6, 2018)
--- WEB/JavaScript/Objeto/classe2.js
+++ WEB/JavaScript/Objeto/classe2.js
@@ -1,19 +1,23 @@
+// Herança com classes
+// Definindo protótipos
+
 class Avo {
     constructor(sobrenome) {
         this.sobrenome = sobrenome
     }
 }
 
-class Pai extends Avo { //Definindo um prototipo com "class". "Avo" é prototipo de "Pai"
+class Pai extends Avo { // "Avo" é protótipo(extende) de "Pai"
     constructor(sobrenome, profissao = 'Professor') {
-        super(sobrenome)
+        super(sobrenome) // "super(<parametro>)" Faz referência à classe protótipo(Avo)
+            // Passa um parâmetro diretamente para o protótipo, que é "Avo"
         this.profissao = profissao
     }
 }
 
 class Filho extends Pai {
     constructor() {
-        super('Silva')
+        super('Silva') // Passando "Silva" de parâmetro para a classe protótipo "Pai"
     }
 }
 
