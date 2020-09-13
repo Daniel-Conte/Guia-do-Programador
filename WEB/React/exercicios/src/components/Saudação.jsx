afde55df356692865702b8c6fbf21d4e9a883591
Daniel Conte <182736@upf.br>
2020-09-13 10:57:44 -0300

    React: removendo texto desnecessarios

--- WEB/React/exercicios/src/components/Saudação.jsx
+++ WEB/React/exercicios/src/components/Saudação.jsx
@@ -1,37 +1,5 @@
 import React, { Component } from 'react'
 
-/*  Componentes de classe tem algumas vantagens sobre os de função:
-        -> state: É um objeto que armazena as propriedades/valores do componente
-            - Ao contrário do "props", no "state" é possível modificar os valores das propriedades
-            - Basicamente, "state" é um "props" que dá pra modificar os valores
-            - Sempre que sofrer uma mudança, o método "render()" é chamado
-
-        -> Ciclo de vida: É possível monitorar e manipular os componentes durante suas 3 fases de vida:
-            https://www.w3schools.com/REACT/react_lifecycle.asp
-
-            --> Mounting: Adiciona o componente na DOM
-                    São executados 4 métodos nessa ordem:
-                    ( "render()" é obrigatório, os outros são opcionais )
-
-                    >> constructor(props) -> É chamado antes de tudo
-                        - Inicializa o state e outros valores
-                        - Antes de tudo chame "super(props)" para incializar os métodos do "Component"
-
-                    >> getDerivedStateFromProps(props, state) -> É chamado logo antes do componente ser renderizado
-                        - É o lugar ideal para setar o state baseado no props
-
-                    >> render() -> Renderiza o componente
-
-                    >> componentDidMount() -> É chamado depois do componente ser renderizado
-                        - É o lugar para manipular algo quando é necessário que o componente já esteja na DOM
-
-            --> Updating: Atualiza o componente
-                    São executados 5 métodos nessa ordem:
-                    ( "render()" é obrigatório, os outros são opcionais )
-
-                    >> getDerivedStateFromProps(props, state) -> É o primeiro método a ser chamado quando um componente é atualizado
-*/
-
 export default class Saudacao extends Component {
     // Uma classe SEMPRE deve extender(definir como pai/prototipo; herda as funções) "Component"
 
