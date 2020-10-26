f54cd10b117ebf7ce15cb3d44ead702494f89a27
Daniel Conte <182736@upf.br>
2020-10-26 09:41:44 -0300

    Crud: Concluindo componente Logo

--- WEB/Projetos/Crud/frontend/src/components/template/Logo.jsx
+++ WEB/Projetos/Crud/frontend/src/components/template/Logo.jsx
@@ -1,8 +1,11 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react'
+import logo from '../../assets/img/logo.png'
 import './Logo.css'
 
 export default props =>
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={logo} alt="Logo"/>
+        </a>
     </aside>
