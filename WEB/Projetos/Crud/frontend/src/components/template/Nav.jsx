74432fac65924a477229492bde52b1cfc4e8b735
Daniel Conte <182736@upf.br>
2020-10-27 10:00:33 -0300

    Crud: Layout do Nav menu concluido

--- WEB/Projetos/Crud/frontend/src/components/template/Nav.jsx
+++ WEB/Projetos/Crud/frontend/src/components/template/Nav.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react'
+import NavLink from './NavLink'
 import './Nav.css'
 
 export default props =>
     <aside className="menu-area">
-        Nav
+        <nav className="menu">
+            <NavLink href="" icon="home" title="Início" />
+            <NavLink href="users" icon="users" title="Usuários" />
+        </nav>
     </aside>
