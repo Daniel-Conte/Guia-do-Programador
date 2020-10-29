bb7088354965e1298e8adf3a049829e554386d0a
Daniel Conte <182736@upf.br>
2020-10-29 10:59:30 -0300

    Crud: Iniciando a lógica do crud

--- WEB/Projetos/Crud/frontend/src/components/user/UserCrud.jsx
+++ WEB/Projetos/Crud/frontend/src/components/user/UserCrud.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import Main from '../template/Main'
 
 const headerProps = {
@@ -7,7 +8,41 @@ const headerProps = {
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir'
 }
 
+const baseUrl = 'http://localhost:3001/users'
+const initialState = {
+    user: { name: '', email: '' },
+    list: []
+}
+
 export default class UserCrud extends Component {
+
+    state = { ...initialState }
+
+    clear() {
+        this.setState({ user: initialState.user })
+    }
+
+    save() {
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+            // Quando for alterar o usuário o id está presente, já quando for adicionar o id não está presente (pois não foi adicionado ainda)
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+
+        axios[method](url, user)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data)
+                this.setState({ user: initialState.user, list })
+            })
+    }
+
+    getUpdatedList(user) {
+        const list = this.state.list.filter(u => u.id !== user.id)
+            // Clona a lista do State com o usuário removido (no caso de "put")
+        list.unshift(user)
+            // Adiciona o usuário no início da lista
+        return list
+    }
+
     render() {
         return (
             <Main {...headerProps}>
