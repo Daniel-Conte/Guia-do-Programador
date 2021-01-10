ed5a51b7f2ce43bb0fdefae30f7bd9627f051d6b
Daniel Conte <182736@upf.br>
2021-01-10 10:56:43 -0300

    calculadora-vue: Implementando a lógica; Finalizando projeto

--- WEB/Projetos/calculadora-vue/src/main/Calculator.vue
+++ WEB/Projetos/calculadora-vue/src/main/Calculator.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="calculator">
-        <Display value="" />
+        <Display :value="displayValue" />
         <Button label="AC" @onClick="clearMemory" triple /> <!-- "onClick" NÃO é um evento padrão, ele foi criado na função "$emit()" do componente -->
         <Button label="/" @onClick="setOperation" operation /> <!-- Já que o evento criado "onClick" retorna a label do componente e a função "setOperation(operation)" está passada sem parâmetros (operation), o valor retornado de "onClick" é passado como parâmetro para a função -->
         <Button label="7" @onClick="addDigit" />
@@ -26,18 +26,64 @@ import Button from '../components/Button'
 import Display from '../components/Display'
 
 export default {
+    data() {
+        return {
+            displayValue: "0",
+            clearDisplay: false,
+            operation: null,
+            values: [0, 0],
+            current: 0
+        }
+    },
     components: { Button, Display },
         // Sempre que for importar um componente, é necessário registrá-lo(incluir no Objeto "components")
     methods: {
-        // Armazena os métodos
         clearMemory() {
-            
+            Object.assign(this.$data, this.$options.data())
+            /*  Controle de estado
+                - "this.$data" retorna os valores ATUAIS de "data" (estado atual)
+                - "this.$options.data()" executa a função "data()" novamente, assim retornando os dados para os valores INICIAIS (estado inicial)
+            */
         },
         setOperation(operation) {
-            
+            if(this.current === 0) {
+                this.operation = operation
+                this.current = 1
+                this.clearDisplay = true
+            } else {
+                const equals = operation === "="
+                const currentOperation = this.operation
+
+                try {
+                    this.values[0] = eval(`${this.values[0]} ${currentOperation} ${this.values[1]}`)
+                } catch(e) {
+                    this.$emit('onError', e)
+                }
+
+                this.values[1] = 0
+                this.displayValue = this.values[0]
+                this.operation = equals ? null : operation
+                this.current = equals ? 0 : 1
+                this.clearDisplay = true
+            }
         },
         addDigit(n) {
-            
+            if(n === '.' && this.displayValue.includes('.')) return
+
+            // Os dados do estado só podem ser acessados com "this", então é possível criar variáveis com o mesmo nome que não vai interferir
+            const clearDisplay = (this.displayValue === '0' && n !== '.') || this.clearDisplay
+            const currentValue = clearDisplay ? "" : this.displayValue
+            const displayValue = currentValue + n
+
+            this.displayValue = displayValue
+                // Pra setar/modificar o estado, basta atribuir um valor
+            this.clearDisplay = false
+
+            if(n !== '.') {
+                const i = this.current
+                const newValue = parseFloat(displayValue)
+                this.values[i] = newValue
+            }
         }
     }
 }
