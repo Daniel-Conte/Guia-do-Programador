0b7b3fec7c4d9fee7b61082f677b6fb0d583106d
Daniel Conte <182736@upf.br>
2021-01-14 10:23:00 -0300

    MontyHall: Criando o formulário e adaptando o jogo conforme os dados do formulário; Finalizando projeto

--- WEB/Projetos/montyhall/src/App.vue
+++ WEB/Projetos/montyhall/src/App.vue
@@ -1,7 +1,25 @@
 <template>
     <div id="app">
         <h1>Problema de Monty Hall</h1>
-        <Door number="1" :hasGift="false" />
+        <div class="form">
+            <div v-if="!started">
+                <label for="doorAmount">Quantas portas?</label>
+                <input id="doorAmount" type="text" size="3" v-model.number="doorAmount">
+            </div>
+            <div v-if="!started">
+                <label for="selectedDoor">Qual porta é premiada?</label>
+                <input id="selectedDoor" type="text" size="3" v-model.number="selectedDoor">
+            </div>
+
+            <button v-if="!started" @click="started = true">Iniciar</button>
+            <button v-if="started" @click="started = false">Reiniciar</button>
+        </div>
+
+        <div class="doors" v-if="started">
+            <div v-for="i in doorAmount" :key="i"> <!-- "key" identifica cada elemento. Usado principalmente em itens de listas. Mesma função que no React -->
+                <Door :number="i" :hasGift="i === selectedDoor" />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,7 +28,14 @@ import Door from './components/Door'
 
 export default {
     name: 'App',
-    components: { Door }
+    components: { Door },
+    data() {
+        return {
+            started: false,
+            doorAmount: 3,
+            selectedDoor: null
+        }
+    }
 }
 </script>
 
@@ -36,5 +61,25 @@ export default {
         background-color: #0004;
         padding: 20px;
         margin-bottom: 60px;
+    }
+
+    .form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 40px;
+    }
+
+    .form, .form input, .form button {
+        margin-bottom: 10px;
+        font-size: 2rem;
+    }
+
+    .doors {
+        align-self: stretch;
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
     }
 </style>
