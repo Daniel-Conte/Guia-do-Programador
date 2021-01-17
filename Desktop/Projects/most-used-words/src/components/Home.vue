60a599320b47944f20dc54642034f037229ae32e
Daniel Conte <182736@upf.br>
2021-01-17 10:20:09 -0300

    most-used-words: Add elemento para fazer upload de arquivos

--- Desktop/Projects/most-used-words/src/components/Home.vue
+++ Desktop/Projects/most-used-words/src/components/Home.vue
@@ -1,5 +1,25 @@
 <template>
     <v-container fluid>
+        <v-form>
+            <v-file-input
+                label="Selecione as Legendas"
+                prepend-icon="mdi-message-text"
+                append-outer-icon="mdi-send"
+                outlined
+                multiple
+                chips
+                v-model="files"
+                @click:append-outer="processSubtitles"
+                 />
+                 <!--
+                     "prepend-icon" -> Ícone que fica na frente(fora) do campo input
+                     "append-outer-icon" -> Ícone que fica atrás(fora) do campo input
+                     "outlined" -> Add borda no campo input
+                     "multiple" -> Permite add vários arquivos
+                     "chips" -> Mostra cada arquivo adicionado dentro de uma "pill"
+                     ":append-outer" -> Vincula o evento "click" à área do ícone atrás(fora) do campo input (append-outer-icon)
+                 -->
+        </v-form>
         <div class="pills">
             <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
         </div>
@@ -19,38 +39,14 @@ export default {
             groupedWords: [
                 { name: 'i', amount: 1234},
                 { name: 'you', amount: 900},
-                { name: 'he', amount: 853},
-                { name: 'i', amount: 1234},
-                { name: 'you', amount: 900},
-                { name: 'he', amount: 853},
-                { name: 'i', amount: 1234},
-                { name: 'you', amount: 900},
-                { name: 'he', amount: 853},
-                { name: 'i', amount: 1234},
-                { name: 'you', amount: 900},
-                { name: 'he', amount: 853},
-                { name: 'i', amount: 1234},
-                { name: 'you', amount: 900},
-                { name: 'he', amount: 853},
-                { name: 'i', amount: 1234},
-                { name: 'you', amount: 900},
-                { name: 'he', amount: 853},
-                { name: 'i', amount: 1234},
-                { name: 'you', amount: 900},
-                { name: 'he', amount: 853},
-                { name: 'i', amount: 1234},
-                { name: 'you', amount: 900},
-                { name: 'he', amount: 853},
-                { name: 'i', amount: 1234},
-                { name: 'you', amount: 900},
-                { name: 'he', amount: 853},
-                { name: 'i', amount: 1234},
-                { name: 'you', amount: 900},
-                { name: 'he', amount: 853},
-                { name: 'i', amount: 1234},
-                { name: 'you', amount: 900},
-                { name: 'he', amount: 853},
-            ]
+                { name: 'he', amount: 853}
+            ],
+            files: []
+        }
+    },
+    methods: {
+        processSubtitles() {
+            console.log(this.files)
         }
     }
 }
