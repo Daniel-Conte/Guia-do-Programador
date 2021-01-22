d6ac84f714a4a5b8df3cc30201f1212577e05e1d
Daniel Conte <182736@upf.br>
2021-01-22 10:07:30 -0300

    Angular: Criando componente header

--- WEB/Angular/Crud/frontend/src/app/app.component.ts
+++ WEB/Angular/Crud/frontend/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+  selector: 'app-root', // Define o nome da tag do componente para ser chamada no HTML
+  templateUrl: 'app.component.html' // Localiza onde está o HTML deste componente
   //template: ` ... `, Usado para escrever o HTML inline
 })
 export class AppComponent {
--- WEB/Angular/Crud/frontend/src/app/app.module.ts
+++ WEB/Angular/Crud/frontend/src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar'; // Algumas coisas precisar ser importadas da biblioteca Material
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule // Sempre importe os módulos usados
   ],
   providers: [],
   bootstrap: [AppComponent]
