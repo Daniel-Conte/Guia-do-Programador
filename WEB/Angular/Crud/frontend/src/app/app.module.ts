606bd974d5f99ece06f981cb8d8888c2d98dc1c6
Daniel Conte <182736@upf.br>
2021-01-23 14:06:41 -0300

    Angular: Criando o componente Footer

--- WEB/Angular/Crud/frontend/src/app/app.module.ts
+++ WEB/Angular/Crud/frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
-import { MatToolbarModule } from '@angular/material/toolbar'; // Algumas coisas precisar ser importadas da biblioteca Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './components/template/footer/footer.component'; // Algumas coisas precisar ser importadas da biblioteca Material
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
