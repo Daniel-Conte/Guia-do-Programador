8be35d9eeca9b8bbcfad1674cedde74598f04f03
Daniel Conte <182736@upf.br>
2021-01-26 11:04:06 -0300

    Angular: Criando o componente Product-crud e configurando o sistema de rotas com Angular Router

--- WEB/Angular/Crud/frontend/src/app/app-routing.module.ts
+++ WEB/Angular/Crud/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component'
+import { ProductCrudComponent } from './views/product-crud/product-crud.component'
+
+const routes: Routes = [{ /* Configura os paths para acessar os componentes e quais componentes serão carregados */
+  path: "", // Define o path que será mostrado na url e que deverá ser acessado pelo "routerLink". "" significa que é a raiz
+  component: HomeComponent // Define qual componente será carregado ao selecionar o path
+}, {
+  path: 'products', // Não coloque "/" na frente do nome, ela é colocada automaticamente na url
+  component: ProductCrudComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
