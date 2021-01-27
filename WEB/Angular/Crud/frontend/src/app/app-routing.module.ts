756ccfc8fcc599a4994d47b94d09e3febb9f6095
Daniel Conte <182736@upf.br>
2021-01-27 10:13:47 -0300

    Angular: Criando o componente ProductCreate e fazendo sua navegação dentro de ProductCrud

--- WEB/Angular/Crud/frontend/src/app/app-routing.module.ts
+++ WEB/Angular/Crud/frontend/src/app/app-routing.module.ts
@@ -3,14 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component'
 import { ProductCrudComponent } from './views/product-crud/product-crud.component'
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
-const routes: Routes = [{ /* Configura os paths para acessar os componentes e quais componentes serão carregados */
+const routes: Routes = [ /* Configura os paths para acessar os componentes e quais componentes serão carregados */
+{
   path: "", // Define o path que será mostrado na url e que deverá ser acessado pelo "routerLink". "" significa que é a raiz
   component: HomeComponent // Define qual componente será carregado ao selecionar o path
-}, {
+},
+{
   path: 'products', // Não coloque "/" na frente do nome, ela é colocada automaticamente na url
   component: ProductCrudComponent
-}];
+},
+{
+  path: 'products/create',
+  component: ProductCreateComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WEB/Angular/Crud/frontend/src/app/app.module.ts
+++ WEB/Angular/Crud/frontend/src/app/app.module.ts
@@ -7,18 +7,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Algumas coisas precisam ser importadas da biblioteca Material
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule} from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
-
-import { MatSidenavModule} from '@angular/material/sidenav'
-import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component'
+
 import { RedDirective } from './directives/red.directive';
-import { MyForDirective } from './directives/for.directive'
+import { MyForDirective } from './directives/for.directive';
 
 @NgModule({
   declarations: [
@@ -29,7 +31,8 @@ import { MyForDirective } from './directives/for.directive'
     HomeComponent,
     ProductCrudComponent,
     RedDirective,
-    MyForDirective
+    MyForDirective,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -38,7 +41,8 @@ import { MyForDirective } from './directives/for.directive'
     MatToolbarModule, // Sempre importe os módulos usados
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
