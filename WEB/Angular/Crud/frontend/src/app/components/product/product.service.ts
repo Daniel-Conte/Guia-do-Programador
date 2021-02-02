8a386b1f4ce92333c53ede0e8b8d5e4befa6a1bb
Daniel Conte <182736@upf.br>
2021-02-02 09:04:54 -0300

    Angular: Fazendo tratamento de erro

--- WEB/Angular/Crud/frontend/src/app/components/product/product.service.ts
+++ WEB/Angular/Crud/frontend/src/app/components/product/product.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar'; // É aquela caixinha que aparece por alguns segundos(geralmente no canto superior direito) para mostrar alguma mensagem (toaster)
 import { HttpClient } from '@angular/common/http'
 import { Product } from './product.model';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root' // "root" torna disponível para todo o módulo e somente uma instância é usada em todo o módulo (Singleton)
@@ -14,34 +15,42 @@ export class ProductService {
 
   create(product: Product): Observable<Product> {
     // Esta função irá retornar um Observable do tipo "Product"(o model)
-    return this.http.post<Product>(this.baseUrl, product)
+    return this.http.post<Product>(this.baseUrl, product).pipe(catchError(e => this.errorHandler(e)))
       // O método "post" é quem realmente retorna um Observable(é a response), então para vincular o retorno deste Observable à função "create()", é preciso retornar o método "post"
   }
 
   read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseUrl)
+    return this.http.get<Product[]>(this.baseUrl).pipe(catchError(e => this.errorHandler(e)))
   }
 
   readById(id: number): Observable<Product> {
     const url = `${this.baseUrl}/${id}`
-    return this.http.get<Product>(url)
+    return this.http.get<Product>(url).pipe(catchError(e => this.errorHandler(e)))
   }
   
   update(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product.id}`
-    return this.http.put<Product>(url, product)
+    return this.http.put<Product>(url, product).pipe(catchError(e => this.errorHandler(e)))
   }
 
   delete(id: number): Observable<Product> {
     const url = `${this.baseUrl}/${id}`
-    return this.http.delete<Product>(url)
+    return this.http.delete<Product>(url).pipe(catchError(e => this.errorHandler(e)))
   }
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: 'right',
-      verticalPosition: 'top'
+      verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success']
+        // "panelClass" aplica uma classe CSS
     })
+  }
+
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Occoreu um erro!', true)
+    return EMPTY
+      // "EMPTY" faz parte do "RxJs" e é um Observable vazio
   }
 }
