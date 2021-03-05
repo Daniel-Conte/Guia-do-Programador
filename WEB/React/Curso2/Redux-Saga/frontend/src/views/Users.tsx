56ef209b8269b0ca9582cad2b848f5ea3add66aa
Daniel Conte <182736@upf.br>
2021-03-05 11:21:04 -0300

    Redux-Saga: Criando a view Users

---
+++ WEB/React/Curso2/Redux-Saga/frontend/src/views/Users.tsx
@@ -0,0 +1,15 @@
+import Main from '../components/template/Main'
+
+const Users: React.FC = () => {
+    return (
+        <Main
+            title="Usuários"
+            subtitle="Cadastro de usuários: Incluir, Listar, Alterar e Excluir"
+            icon="users"
+        >
+
+        </Main>
+    )
+}
+
+export default Users
