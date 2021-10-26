9399778a3a16eef621a7e1e18e8ffc81b5922b17
Daniel Conte <182736@upf.br>
2021-10-25 21:55:43 -0300

    Express: Explicacao dos middlewares 1

--- WEB/Express/exercicios-express/index.js
+++ WEB/Express/exercicios-express/index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const app = express();
 // Para iniciar uma aplicação express, deve-se importar o módulo e executá-lo
 
-// "app.use(<endpoint>, <callback>)" É um middleware que é executado sempre que uma requisição é enviada para o servidor
+// "app.use(<endpoint>, <callback>)" É um middleware que é executado sempre que uma requisição é enviada para o servidor(dependendo de como está organizada a cadeia de middlewares)
 // Trata qualquer tipo de requisição(GET, POST, PUT...)
 // Se não tiver um endpoint, todas as requisições passarão por esse middleware
-app.use((req, res) => {
+// Geralmente é utilizado pra fazer um tratamento básico nas requisições e joga ela pro próximo middleware
+app.use((req, res, next) => {
+  // O terceiro parâmetro da callback é uma função que ao ser executada dá continuidade à cadeira de middlewares, executando o próximo da cadeia
+  // É parecido com o encadeamento de ".then()" nas Promises
+  // !!! A ordem da cadeia de middlewares é determinada pela ordem de escrita no código !!!
+  console.log('Antes...');
+  next();
+  // Executa o próximo middleware
+  // Se não executar a função "next", a cadeia para
+});
+
+app.use((req, res, next) => {
+  console.log('Durante...');
   //res.send('<b>Bueno<b/>');
   // "res.send(<resposta>)" envia uma response para quem fez a requisição
   // O send interpreta HTML
@@ -26,6 +38,13 @@ app.use((req, res) => {
     limit: 3,
     status: 200,
   });
+
+  next();
+});
+
+app.use((req, res) => {
+  console.log('Depois...');
+  // Por não estar dando continuidade ("next()"), a cadeia para aqui e não chama as funções abaixo (mesmo se o endpoint estiver certo)
 });
 
 // Quando usa um endpoint, o middleware será executado somente se a url iniciar com o endpoint especificado
