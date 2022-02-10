db7bedf33c834f88849eb49993ffe8de6364d72a
Daniel Conte <182736@upf.br>
2022-02-10 00:37:31 -0300

    Next: Componentes

--- WEB/Next/fundamentos/src/pages/estiloso.jsx
+++ WEB/Next/fundamentos/src/pages/estiloso.jsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
 import styles from '../styles/estiloso.module.css';
 // O módulo CSS deve ser importado e usado como um objeto
 
 export default function Estiloso() {
   return (
     <div className={styles.roxo}>
+      <Link href='/'>Voltar</Link>
       <h1>Estilo usando CSS Módulos</h1>
     </div>
   );
