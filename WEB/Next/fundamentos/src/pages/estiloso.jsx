dedab6b9e7b71d509aa70b82f9cc2bc4fd4ff524
Daniel Conte <182736@upf.br>
2022-02-11 00:00:13 -0300

    Next: Componente layout

--- WEB/Next/fundamentos/src/pages/estiloso.jsx
+++ WEB/Next/fundamentos/src/pages/estiloso.jsx
@@ -1,12 +1,13 @@
-import Link from 'next/link';
+import Layout from '../components/Layout';
 import styles from '../styles/estiloso.module.css';
 // O módulo CSS deve ser importado e usado como um objeto
 
 export default function Estiloso() {
   return (
-    <div className={styles.roxo}>
-      <Link href='/'>Voltar</Link>
-      <h1>Estilo usando CSS Módulos</h1>
-    </div>
+    <Layout titulo='Exemplo de CSS modularizado'>
+      <div className={styles.roxo}>
+        <h1>Estilo usando CSS Módulos</h1>
+      </div>
+    </Layout>
   );
 }
