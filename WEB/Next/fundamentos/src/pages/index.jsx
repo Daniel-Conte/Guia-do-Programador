e4f504b9458ead1d152f277e9330bdbe35a401e7
Daniel Conte <182736@upf.br>
2022-02-15 00:27:16 -0300

    Next: Integracao com API

--- WEB/Next/fundamentos/src/pages/index.jsx
+++ WEB/Next/fundamentos/src/pages/index.jsx
@@ -15,7 +15,8 @@ export default function Inicio() {
       <Navegador destino='/jsx' texto='JSX' cor='crimson' />
       <Navegador destino='/navegacao' texto='Navegação #01' cor='green' />
       <Navegador destino='/cliente/rs-2/321' texto='Navegação #02' cor='blue' />
-      <Navegador destino='/estado' texto='Componente com estado' cor='pink' />
+      <Navegador destino='/estado' texto='Componente com estado' cor='#a45b71' />
+      <Navegador destino='/integracao_1' texto='Integracao com API #01' cor='#42a9a9' />
     </div>
   );
 }
