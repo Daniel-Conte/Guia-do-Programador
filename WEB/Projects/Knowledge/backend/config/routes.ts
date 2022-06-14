c9c59abed160ae0a973e6465adaef8ba9eae9907
Daniel Conte <182736@upf.br>
2022-06-14 00:36:10 -0300

    Knowledge: valida funcoes de administradores

--- WEB/Projects/Knowledge/backend/config/routes.ts
+++ WEB/Projects/Knowledge/backend/config/routes.ts
@@ -1,3 +1,5 @@
+import admin from './admin';
+
 import type { ExpressConsign } from '../types';
 import type { RoutesArticles, RoutesAuth, RoutesCategories, RoutesUsers } from './routes.types';
 
@@ -9,19 +11,19 @@ module.exports = (app: ExpressConsign) => {
   app
     .route<RoutesUsers>('/users')
     .all(app.config.passport.authenticate())
-    .post(app.api.user.save)
-    .get(app.api.user.get);
+    .post(admin(app.api.user.save))
+    .get(admin(app.api.user.get));
   app
     .route<RoutesUsers>('/users/:id')
     .all(app.config.passport.authenticate())
-    .put(app.api.user.save)
-    .get(app.api.user.getById);
+    .put(admin(app.api.user.save))
+    .get(admin(app.api.user.getById));
 
   app
     .route<RoutesCategories>('/categories')
     .all(app.config.passport.authenticate())
-    .get(app.api.category.get)
-    .post(app.api.category.save);
+    .get(admin(app.api.category.get))
+    .post(admin(app.api.category.save));
   app
     .route<RoutesCategories>('/categories/tree')
     .all(app.config.passport.authenticate())
@@ -30,20 +32,20 @@ module.exports = (app: ExpressConsign) => {
     .route<RoutesCategories>('/categories/:id')
     .all(app.config.passport.authenticate())
     .get(app.api.category.getById)
-    .put(app.api.category.save)
-    .delete(app.api.category.remove);
+    .put(admin(app.api.category.save))
+    .delete(admin(app.api.category.remove));
 
   app
     .route<RoutesArticles>('/articles')
     .all(app.config.passport.authenticate())
-    .get(app.api.article.get)
-    .post(app.api.article.save);
+    .get(admin(app.api.article.get))
+    .post(admin(app.api.article.save));
   app
     .route<RoutesArticles>('/articles/:id')
     .all(app.config.passport.authenticate())
     .get(app.api.article.getById)
-    .put(app.api.article.save)
-    .delete(app.api.article.remove);
+    .put(admin(app.api.article.save))
+    .delete(admin(app.api.article.remove));
   app
     .route<RoutesArticles>('/categories/:id/articles')
     .all(app.config.passport.authenticate())
