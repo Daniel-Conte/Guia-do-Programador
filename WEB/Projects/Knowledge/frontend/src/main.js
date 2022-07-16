1f23b9e02c7d33af08d0d318dd446ca748ecdaad
Daniel Conte <182736@upf.br>
2022-07-16 11:45:57 -0300

    Knowledge: Cria dashboard e chama api estatisticas

--- WEB/Projects/Knowledge/frontend/src/main.js
+++ WEB/Projects/Knowledge/frontend/src/main.js
@@ -9,6 +9,10 @@ import router from './config/router';
 
 Vue.config.productionTip = false;
 
+// TEMP
+require('axios').defaults.headers.common['Authorization'] =
+  'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkRhbmllbCBDIiwiZW1haWwiOiJ0ZXN0ZUB0ZXN0ZS5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjU3OTgxODA2LCJleHAiOjE2NTgyNDEwMDZ9.yEZce1fyMW07DPuYiXQ-GobaqwzO7tIlxtKlg194ngw';
+
 new Vue({
   store,
   router,
