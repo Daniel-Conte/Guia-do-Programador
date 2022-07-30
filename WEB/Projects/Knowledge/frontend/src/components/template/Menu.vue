4b1a2ae8b0e0675404d7fb7c50e5bc7d34fd1be5
Daniel Conte <182736@upf.br>
2022-07-30 00:18:26 -0300

    Knowledge: Cria arvore de categorias no menu

--- WEB/Projects/Knowledge/frontend/src/components/template/Menu.vue
+++ WEB/Projects/Knowledge/frontend/src/components/template/Menu.vue
@@ -1,13 +1,55 @@
 <template>
-  <aside class="menu" v-show="isMenuVisible"></aside>
+  <aside class="menu" v-show="isMenuVisible">
+    <div class="menu-filter">
+      <i class="fa fa-search fa-lg"></i>
+      <input
+        type="text"
+        placeholder="Digite para filtrar..."
+        v-model="treeFilter"
+        class="filter-field"
+      />
+    </div>
+    <Tree :data="treeData" :options="treeOptions" :filter="treeFilter" ref="tree" />
+  </aside>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import Tree from 'liquor-tree';
+import { baseApiUrl } from '@/global';
+import axios from 'axios';
 
 export default {
   name: 'MainMenu',
+  components: { Tree },
   computed: mapState(['isMenuVisible']),
+  data: function () {
+    return {
+      treeFilter: '',
+      treeData: this.getTreeData(),
+      treeOptions: {
+        propertyNames: { text: 'name' },
+        filter: { emptyText: 'Categoria não encontrada' },
+      },
+    };
+  },
+  methods: {
+    getTreeData() {
+      const url = `${baseApiUrl}/categories/tree`;
+      return axios.get(url).then(res => res.data);
+    },
+    onNodeSelect(node) {
+      this.$router.push({
+        name: 'articlesByCategory',
+        params: { id: node.id },
+      });
+    },
+  },
+  mounted() {
+    // "$refs" acessa os componentes com "ref"
+    // "node:selected" é o evento de selecionar um node
+    this.$refs.tree.$on('node:selected', this.onNodeSelect);
+  },
 };
 </script>
 
@@ -19,5 +61,49 @@ export default {
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
+}
+
+.menu span,
+.menu span:hover {
+  color: #fff;
+}
+
+.menu .tree-node.selected > .tree-content,
+.menu .tree-node:not(.selected) > .tree-content:hover {
+  background-color: rgba(2525, 255, 255, 0.2);
+}
+
+.tree-arrow.has-child {
+  filter: brightness(2);
+}
+
+.menu .menu-filter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin: 20px;
+  padding-bottom: 8px;
+  border-bottom: 1px solid #aaa;
+}
+
+.menu .menu-filter i {
+  color: #aaa;
+  margin-right: 10px;
+}
+
+.menu input {
+  color: #ccc;
+  font-size: 1.3rem;
+  border: 0;
+  outline: 0;
+  width: 100%;
+  background: transparent;
+}
+
+.tree-filter-empty {
+  color: #ccc;
+  font-size: 1.3rem;
+  margin-left: 20px;
 }
 </style>
