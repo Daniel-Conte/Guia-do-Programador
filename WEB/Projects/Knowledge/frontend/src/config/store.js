05caf9bda24b2f67a63915193c76f1fbd770c6e4
Daniel Conte <182736@upf.br>
2022-07-30 00:19:11 -0300

    Knowledge: Altera menu e store para lidar com o usuario

--- WEB/Projects/Knowledge/frontend/src/config/store.js
+++ WEB/Projects/Knowledge/frontend/src/config/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 // Vuex é um gerenciador de estado para o Vue como o Redux é para o React
 
@@ -8,19 +9,32 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   // state armazena o initial values da store
   state: {
-    isMenuVisible: true,
-    user: {
-      name: 'Usuário Mock',
-      email: 'mock@mock.com.br',
-    },
+    isMenuVisible: false,
+    user: null,
   },
   // mutations funciona como o reducer do Redux
   mutations: {
     toggleMenu(state, isVisible) {
+      if (!state.user) {
+        state.isMenuVisible = false;
+        return;
+      }
+
       if (isVisible === undefined) {
         state.isMenuVisible = !state.isMenuVisible;
       } else {
         state.isMenuVisible = isVisible;
+      }
+    },
+    setUser(state, user) {
+      state.user = user;
+
+      if (user) {
+        axios.defaults.headers.common['Authorization'] = `bearer ${user.token}`;
+        state.isMenuVisible = true;
+      } else {
+        delete axios.defaults.headers.common['Authorization'];
+        state.isMenuVisible = false;
       }
     },
   },
