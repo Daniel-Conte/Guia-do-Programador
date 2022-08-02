43551b674fdbe12563cfaa29a7afc8b866c73678
Daniel Conte <182736@upf.br>
2022-08-02 00:06:57 -0300

    Knowledge: Cria tela de login e cadastro

--- WEB/Projects/Knowledge/frontend/src/main.js
+++ WEB/Projects/Knowledge/frontend/src/main.js
@@ -12,7 +12,7 @@ Vue.config.productionTip = false;
 
 // TEMP
 require('axios').defaults.headers.common['Authorization'] =
-  'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkRhbmllbCBDIiwiZW1haWwiOiJ0ZXN0ZUB0ZXN0ZS5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjU4ODg5NTk1LCJleHAiOjE2NTkxNDg3OTV9.lMoakEwjlLx223KcAywV-Y9QX8sjsknJS_pahF5ziXI';
+  'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkRhbmllbCBDIiwiZW1haWwiOiJ0ZXN0ZUB0ZXN0ZS5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjU5NDA4MDU4LCJleHAiOjE2NTk2NjcyNTh9.3w-PAC2KtIRKNr-T76kJINkV_b5bcImD0HnFX3RZUWs';
 
 new Vue({
   store,
